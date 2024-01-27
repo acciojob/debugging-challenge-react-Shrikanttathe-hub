251db47f6d9b29f48e4127bcf20b2a4c6265bcac
shrikant_tathe <sptathe@gmail.com>
2024-01-27 23:42:09 +0530

    nasa

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, } from "react";
 import '../styles/App.css';
 
 const App = () => {
 
-  let [count, setCount] = useState(0)
+  const [count, setCount] = useState(0)
 
   return (
     <div class="ball">
-      <h1 class="count" ondoubleclick={() => { alert("cant edit it") }}>{count}</h1>
-      <button class='increment-button' onclick={() => { setCount(count + 1) }}>Increment</button>
+      <h1 class="count" onDoubleClick={() => { alert("cant edit it") }}>{count}</h1>
+      <button class='increment-button' onClick= {() => {count(setCount + 1) }}>Increment</button>
     </div>
   )
 }
